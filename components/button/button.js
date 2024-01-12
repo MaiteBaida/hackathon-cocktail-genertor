67f8ec2336f71104820a0971c1d7696c518963be
Maite Baida <maitemsabaida@gmail.com>
2024-01-12 10:12:16 -0700

    styles

---
+++ components/button/button.js
@@ -0,0 +1,6 @@
+//Gala
+const Button = (buttonText, isDisabled = false) => {
+    return `<button class="button" ${isDisabled ? 'disabled' : ''}>${buttonText}</button>`;
+};
+
+  export default Button;
--- components/main/main.js
+++ components/main/main.js
@@ -1,21 +1,28 @@
+//Gala
+import Button from '../button/button.js';
+
 const Main = () => {
-return `
-<main class='form'>
-      <form action="">
-        <div>
-          <label for="season" class='form__label'>Pick your season:</label>
-          <select name="formSeason" class='form__selector'>
-            <option value="Winter">Winter</option>
-            <option value="Spring">Spring</option>
-            <option value="Summer">Summer</option>
-            <option value="Fall">Fall</option>
-          </select>
-        </div>
-        <button type="submit" class='form__button'>Submit</button>
-        <div></div>
-      </form>
-    </main>
-`
+    return `
+        <main>
+            <form class="form">
+                <div class='form__container'>
+                    <label for="form__season"></label>
+                    <select name="form__season" class="season__list">
+                        <option value="" disabled selected>Select a season</option>
+                        <option value="Winter">Winter</option>
+                        <option value="Spring">Spring</option>
+                        <option value="Summer">Summer</option>
+                        <option value="Fall">Fall</option>
+                    </select>
+                <div>
+                    ${Button('Generate your cocktail')}
+                    </div>
+                    <div class="cocktail__container"></div>
+                </div>
+                </div>
+            </form>
+        </main>
+    `;
 }
 
 export default Main;
